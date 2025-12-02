060d1437892ac431fc95672170bbbf8f0b736f9a
soumya19991 <74457972+soumya19991@users.noreply.github.com>
2025-12-02 15:50:08 +0530

    done

--- resources/views/layout/student/partials/footer.blade.php
+++ resources/views/layout/student/partials/footer.blade.php
@@ -1,20 +1,3 @@
-<footer class="footer footer-transparent d-print-none">
-    <div class="container-xl">
-        <div class="flex-row-reverse text-center row align-items-center">
-            <div class="mt-3 col-12 col-lg-auto mt-lg-0">
-                <ul class="mb-0 list-inline list-inline-dots">
-                    {{-- <li class="list-inline-item">
-                        Copyright &copy; {{ date('Y') }}
-                        <a href="#" class="link-secondary">Superio</a>.
-                        All rights reserved.
-                    </li> --}}
-                    {{-- <li class="list-inline-item">
-                        <a href="{{ route('home') }}" class="link-secondary" rel="noopener">
-                            v1.0.0-beta19
-                        </a>
-                    </li> --}}
-                </ul>
-            </div>
-        </div>
-    </div>
+<footer style="text-align:center; padding:15px; background:#ecf0f1; margin-top:20px; overflow-x:hidden;">
+    © {{ date('Y') }} Career & Courses — All rights reserved.
 </footer>
